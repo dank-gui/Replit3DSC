e42f60207681aa4b9f0a8ae5712573fe3f61019a
Massaroo <130477890+dank-gui@users.noreply.github.com>
2024-10-17 08:53:50 -0300

    DesafioModuloPersonagem

---
+++ DesafioModuloPersonagem/index.js
@@ -0,0 +1,11 @@
+// CRIE UM MODULO ESM "personagem.js"
+// CRIE FUNÇÕES PARA:
+// IMPRIMIR UM OBJETO COM (MARCA, NOME, ANO)
+// EXPORTE A FUNÇÃO (ImprimirDadosPersonagens) PARA OUTRO MÓDULO
+// IMPORTE O MÓDULO (ImprimirDadosPerson) NO SEU ARQUIV0
+// INDEX.JS
+// UTILIZE O MÓDULO IMPORTADO
+
+import {patch, f} from "./lib/personagens.js";
+
+console.log
